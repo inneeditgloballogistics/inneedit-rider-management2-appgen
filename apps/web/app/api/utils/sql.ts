7c1e20cd001c36219abd0e343ee14aa2c7c598b5
inneeditgloballogistics <inneeditgloballogistics@gmail.com>
2026-03-04 16:42:19 +0530

    in store map view for each pin i want daisplay of store name beside pin

--- apps/web/app/api/utils/sql.ts
+++ apps/web/app/api/utils/sql.ts
@@ -1,10 +1,10 @@
-import { sql as neonSql } from '@neondatabase/serverless';
+import { neon } from '@neondatabase/serverless';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set');
 }
 
-const sql = neonSql(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL);
 
 export { sql };
 export default sql;
